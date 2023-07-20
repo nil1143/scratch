00719fec55741bb86699baf73211223534a960ad
nil1143 <tnil1143@gmail.com>
2023-07-20 16:53:50 +0100

    patch
    20/07

---
+++ public/index.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('#nav')
+
+hamburger.addEventListener('click', () => {
+   
+    if(nav.classList.contains('hidden')) {
+        nav.classList.remove('hidden')
+    } else {
+        nav.classList.add('hidden')
+    }})
+
+const navLink= document.querySelectorAll('.nav-link')
+
+
+navLink.forEach(link => link.addEventListener('click', () => {
+    nav.classList.add('hidden')
+}))
+
